5a8b928abb89d8a355fa37ad38ce1f400a3af46f
Ben Coleman <colemabs@miamioh.edu>
2016-08-22 22:30:19 -0400

    done with exercise 3, calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,42 @@
 #write your code here
+def add(a, b)
+	a + b
+end
+
+def subtract(a, b)
+	a - b
+end
+
+def sum(array)
+	
+	total = 0
+	array.each do |i|
+		total = total + i
+	end
+	return total
+end
+
+def multiply(array)
+	total = 1
+	array.each do |i|
+		total = total * i
+	end
+	return total
+end
+
+def power(a , b)
+	
+	total = 1
+	for i in 1..b
+		total = total * a
+	end
+	return total
+end
+
+def factorial(num)
+if num == 0
+		1
+else
+	 num * factorial(num - 1)
+	end
+end
